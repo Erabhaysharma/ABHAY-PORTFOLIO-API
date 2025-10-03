c5c75566531898339c99e9f3f3e6f9b58954c3e2
Abhay kumar <abhaysharma75547@gmail.com>
2025-10-03 20:22:22 +0530

    prostrage sqls

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from sqlalchemy import create_engine, Column, String, Integer, JSON
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
 # -------------------- Database --------------------
-DATABASE_URL = os.getenv("DATABSE_URL")  # Render Postgres URL
+DATABASE_URL = os.getenv("DATABASE_URL")  # Render Postgres URL
+print("DATABASE_URL =", os.getenv("DATABASE_URL"))
+
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
